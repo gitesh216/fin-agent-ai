ba97885ac4d3848ff6e5c0248bc2ce929b1a1e0f
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-08-20 11:18:57 +0530

    feat: add groq sdk and boilerplate code

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function callAgent() {
+  const completion = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "Hello, how are you?",
+      },
+    ],
+    model: "llama-3.3-70b-versatile",
+  });
+
+  console.log(completion.choices[0]?.message?.content || "");
+}
