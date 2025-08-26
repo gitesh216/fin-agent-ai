ac7c1a60f89be7cbb00e1b5d056815eac311233d
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-08-26 20:38:19 +0530

    feat: add tool call for agent

--- index.ts
+++ index.ts
@@ -3,54 +3,130 @@ import Groq from "groq-sdk";
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 interface ExpenseDate {
-    from: string;
-    to: string;
+  from: string;
+  to: string;
 }
 
-function getTotalExpense({from, to} : ExpenseDate) {
-    console.log("Calling getTotalExpense Tool");
-    console.log(from, to);
+function getTotalExpense({ from, to }: ExpenseDate) {
+  console.log("Calling getTotalExpense Tool");
+  console.log(from, to);
+  return 1000;
 }
 
 async function callAgent() {
-  const completion = await groq.chat.completions.create({
-    messages: [
-      {
-        role: "system",
-        content: "You are a savvy, trustworthy personal finance expert with a friendly and practical tone. Your goal is to help users make smart financial decisions by offering clear, actionable advice on budgeting, saving, investing, debt management, and financial planning.",
-      },
-      {
-        role: "user",
-        content: "What are my total expense of this month",
-      }
-    ],
-    model: "llama-3.3-70b-versatile",
-    tools: [
-        {
-            type: 'function',
-            function: {
-                name: 'getTotalExpense',
-                description: 'Get total expense from date to date',
-                parameters: {
-                    type: 'object',
-                    properties: {
-                        from: {
-                            type: 'string',
-                            description: 'From date to get total expense'
-                        },
-                        to: {
-                            type: 'string',
-                            description: 'To date to get total expense'
-                        }
-                    },
-                    required: ['from', 'to']
-                }
-            }
-        }
-    ]
+  const messages: any[] = [
+    {
+      role: "system",
+      content: `You are a savvy, trustworthy personal finance expert with a friendly and practical tone. Your goal is to help users make smart financial decisions by offering clear, actionable advice on budgeting, saving, investing, debt management, and financial planning. Current date time: ${new Date().toUTCString()}`,
+    },
+  ];
+
+  messages.push({
+    role: "user",
+    content: "What are my total expense of this month",
   });
 
-  console.log(completion.choices[0]?.message?.content  || "");
+  const completion = await groq.chat.completions.create({
+    messages: messages,
+    model: "llama-3.3-70b-versatile",
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "getTotalExpense",
+          description: "Get total expense from date to date",
+          parameters: {
+            type: "object",
+            properties: {
+              from: {
+                type: "string",
+                description: "From date to get total expense",
+              },
+              to: {
+                type: "string",
+                description: "To date to get total expense",
+              },
+            },
+            required: ["from", "to"],
+          },
+        },
+      },
+    ],
+  });
+
+  console.log(
+    "First completion:",
+    JSON.stringify(completion.choices[0], null, 2)
+  );
+
+  messages.push(completion.choices[0]?.message);
+
+  const toolCalls = completion.choices[0]?.message?.tool_calls || [];
+
+  if (!toolCalls.length) {
+    console.log(`Assistant: ${completion?.choices[0]?.message.content || ""}`);
+    return;
+  }
+
+  for (const tool of toolCalls) {
+    const functionName = tool.function?.name;
+    const functionArgs = tool.function?.arguments;
+
+    let result = "";
+
+    if (functionName === "getTotalExpense") {
+      result = String(getTotalExpense(JSON.parse(functionArgs)));
+    }
+
+    messages.push({
+      role: "assistant",
+      content: null,
+      tool_calls: [tool],
+    });
+
+    messages.push({
+      role: "tool",
+      content: result,
+      tool_call_id: tool.id,
+    });
+
+    const completion2 = await groq.chat.completions.create({
+      messages,
+      model: "llama-3.3-70b-versatile",
+      tools: [
+        {
+          type: "function",
+          function: {
+            name: "getTotalExpense",
+            description: "Get total expense from date to date",
+            parameters: {
+              type: "object",
+              properties: {
+                from: {
+                  type: "string",
+                  description: "From date to get total expense",
+                },
+                to: {
+                  type: "string",
+                  description: "To date to get total expense",
+                },
+              },
+              required: ["from", "to"],
+            },
+          },
+        },
+      ],
+    });
+
+    console.log(
+      "Final completion: ",
+      JSON.stringify(completion2.choices[0], null, 2)
+    );
+  }
+  console.log("=======================");
+  console.log("Messages", JSON.stringify(messages, null, 2));
+  
+  
 }
 
 callAgent();
